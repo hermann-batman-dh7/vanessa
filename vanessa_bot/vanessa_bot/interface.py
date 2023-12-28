f091196a372e1a6af089c8e9ecdfe0d9574c5ed3
rosszin <145496791+DevJr-Ross@users.noreply.github.com>
2023-12-28 16:56:59 +0100

    Atualizando...

---
+++ vanessa_bot/vanessa_bot/interface.py
@@ -0,0 +1,34 @@
+import pyttsx3 
+from tkinter import *
+from main import mainact
+
+janela = Tk()
+janela.title("Vanessa")
+
+largura_janela = 300
+altura_janela = 300
+posicao_x = (janela.winfo_screenwidth() // 2) - (largura_janela // 2)
+posicao_y = (janela.winfo_screenheight() // 2) - (altura_janela // 2)
+
+janela.geometry(f"{largura_janela}x{altura_janela}+{posicao_x}+{posicao_y}")
+janela.resizable(False, False)
+
+# Carregando e exibindo a imagem
+imagem = PhotoImage(file="cover.png")
+label_imagem = Label(janela, image=imagem)
+label_imagem.pack(pady=20)
+
+texto_orientacao = Label(janela, text="Fale com a Vanessa")
+texto_orientacao.pack(pady=10)  # Ajustei o padding para acomodar a imagem
+
+botao = Button(janela, text="Clique Para iniciar uma conversa", command=mainact)
+botao.pack(pady=0)
+
+'''entrada_texto = Entry(janela, width=50)
+entrada_texto.pack(pady=20)
+
+# Botão para enviar a entrada
+botao = Button(janela, text="Enviar", command=mainact)
+botao.pack(pady=20)'''
+
+janela.mainloop()
--- vanessa_bot/vanessa_bot/main.py
+++ vanessa_bot/vanessa_bot/main.py
@@ -7,54 +7,123 @@ import pyttsx3
 import json
 from vosk import Model, KaldiRecognizer
 
-
-#função de speak da máquina
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[-2].id)
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
-
-while True:
-
-        text = ""
-        speak("O que você deseja: ")
-        text = input("O que você deseja: ")
-        print(text)
+'''def mainact():
+    #função de speak da máquina
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[-2].id)
     
-        if text == 'email':
-            bot_instance = Bot()
-            bot_instance.sendmail()
+    def speak(text):
+        engine.say(text)
+        engine.runAndWait()
+        
+    def recognize_speech():
+        recognizer = sr.Recognizer()
+
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
+        try:
+            text = recognizer.recognize_google(audio, language='pt-BR')
+            return text
+        except sr.UnknownValueError:
+            print("Desculpe, não foi possível entender o áudio.")
+            Bot.speak("Desculpe, não foi possível entender o áudio.")
+            return ""
+        except sr.RequestError as e:
+            print(f"Não foi possível obter resultados do serviço de Reconhecimento de Fala do Google.")
+            return ""
+        
+    text = ""
+    speak("O que você deseja: ")
+    text = input("O que você deseja: ")
+    print(text)
+    text = recognize_speech'''
+
+'''if text == 'email':
+        bot_instance = Bot()
+        bot_instance.sendmail()
+        
+    if text == 'spotify':
+        bot_instance = Bot()
+        bot_instance.spotify()
+        
+    if text == 'explorador':
+        bot_instance = Bot()
+        bot_instance.explorador()
+        
+    if text == 'google':
+        bot_instance = Bot()
+        bot_instance.webbrow()
+        
+    horas = text     
+    keywords = ['hora', 'que horas', 'diga a hora', 'diz as horas', 'que horas são'] 
+    keyword_present = any(keyword in horas for keyword in keywords)    
+    if keyword_present:
+            speak(datahora.SystemInfo.get_time())
             
-        if text == 'spotify':
-            bot_instance = Bot()
-            bot_instance.spotify()
             
-        if text == 'explorador':
-             bot_instance = Bot()
-             bot_instance.explorador()
-             
-        if text == 'google':
-             bot_instance = Bot()
-             bot_instance.webbrow()
-             
-        horas = text     
-        keywords = ['hora', 'que horas', 'diga a hora', 'diz as horas', 'que horas são'] 
-        keyword_present = any(keyword in horas for keyword in keywords)    
-        if keyword_present:
-                speak(datahora.SystemInfo.get_time())
-                
-                
-        data = text     
-        keywords = ['data', 'qual a data de hoje', 'diga a data', 'diz a data de hoje'] 
-        keyword_present = any(keyword in horas for keyword in keywords)        
-        if keyword_present:
-                speak(datahora.SystemInfo.get_date())
-                
-        clima = text     
-        keywords = ['clima', 'qual o clima de hoje', 'diga o clima', 'diz o clima de hoje'] 
-        keyword_present = any(keyword in horas for keyword in keywords)        
-        if keyword_present:
-                city = 'Lubango'
-                speak(datahora.SystemInfo.get_weather(city))
+    data = text     
+    keywords = ['data', 'qual a data de hoje', 'diga a data', 'diz a data de hoje'] 
+    keyword_present = any(keyword in horas for keyword in keywords)        
+    if keyword_present:
+            speak(datahora.SystemInfo.get_date())
+            
+    clima = text     
+    keywords = ['clima', 'qual o clima de hoje', 'diga o clima', 'diz o clima de hoje'] 
+    keyword_present = any(keyword in horas for keyword in keywords)        
+    if keyword_present:
+            city = 'Lubango'
+            speak(datahora.SystemInfo.get_weather(city))
+            
+if __name__ == "__main__":
+    mainact()'''
+
+'''def mainact():
+    # Inicialização da engine para falar
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[-2].id)
+    
+    # Função para falar
+    def speak(text):
+        engine.say(text)
+        engine.runAndWait()
+
+    # Função para reconhecimento de fala
+    def recognize_speech():
+        recognizer = sr.Recognizer()
+
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
+            
+        try:
+            text = recognizer.recognize_google(audio, language='pt-BR')
+            return text
+        except sr.UnknownValueError:
+            speak("Desculpe, não foi possível entender o áudio.")
+            return None
+        except sr.RequestError as e:
+            speak("Não foi possível obter resultados do serviço de Reconhecimento de Fala do Google.")
+            return None
+
+    speak("Você gostaria de falar ou digitar? Diga 'falar' ou 'digitar'.")
+    choice = input("Você gostaria de falar ou digitar? (falar/digitar): ").lower()
+
+    if choice == 'falar':
+        text = recognize_speech()
+        while not text:
+            speak("Por favor, repita sua solicitação.")
+            text = recognize_speech()
+    elif choice == 'digitar':
+        speak("O que você deseja?")
+        text = input("O que você deseja: ")
+    else:
+        speak("Opção não reconhecida.")
+
+    if text:
+        print("Sua solicitação:", text)
+
+if __name__ == "__main__":
+    mainact()'''
