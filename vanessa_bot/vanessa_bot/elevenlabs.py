6c0fe72f99b67afd2f13646e0606f91e66269d9b
Agatão <agataodoriafrancisco91@gmail.com>
2024-01-15 10:15:32 +0100

    go

---
+++ vanessa_bot/vanessa_bot/elevenlabs.py
@@ -0,0 +1,11 @@
+from elevenlabs import clone, generate, play
+
+voice = clone(
+    name="Alex",
+    description="An old American male voice with a slight hoarseness in his throat. Perfect for news", # Optional
+    files=["./sample_0.mp3", "./sample_1.mp3", "./sample_2.mp3"],
+)
+
+audio = generate(text="Hi! I'm a cloned voice!", voice=voice)
+
+play(audio)
